5a07dc8b94ef150ffd3518b492c2717825ff09e9
EC2 Default User <ec2-user@ip-172-31-45-49.ap-northeast-1.compute.internal>
2020-03-06 22:59:54 +0000

    回答するを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :groups, only: [:show, :edit, :update]
   resources :questions, only: [:create]
+  resources :answers, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
