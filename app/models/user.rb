ecb58a372aa6ad02bcf14fd0301cca7ec7c6fcb9
EC2 Default User <ec2-user@ip-172-31-45-49.ap-northeast-1.compute.internal>
2020-03-06 14:31:48 +0000

    updataのviewの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,10 @@ class User < ApplicationRecord
   def name_kana
     "#{family_name_kana} #{first_name_kana}"
   end
+  
+  def full_profile?
+    image.attached?  && family_name? && first_name? && family_name_kana? && first_name_kana?
+  end
 
   private
   def has_group_key?
