f8a9f3d6956edf03ad541db857ec7a0834d93eb2
EC2 Default User <ec2-user@ip-172-31-45-49.ap-northeast-1.compute.internal>
2020-03-06 15:00:58 +0000

    グループ機能実施

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: "top#index"
   
   resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
